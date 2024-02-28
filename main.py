f8fddc7c14ea6b4c0ab9e75d7c62aea94e771cdf
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-02-28 23:28:48 +0200

    fixed syntax errors in paddle and key listeners

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 
-# TODO: Create screen
-
-# TODO: Create another Paddle
 # TODO: Collisions
 # TODO: Create moving ball
 # TODO: detect paddle miss
@@ -14,11 +11,17 @@ screen.setup(width=800, height=800)
 screen.title("Pong")
 screen.tracer(0)
 
-
 # Create Paddles
 right_paddle = Paddle(350, 0)
 left_paddle = Paddle(-350, 0)
 
+
+#Controls
+screen.listen()
+screen.onkey(right_paddle.go_up,"i")
+screen.onkey(right_paddle.go_down, "k")
+screen.onkey(left_paddle.go_up,"w")
+screen.onkey(left_paddle.go_down, "s")
 
 # Gameplay
 game_is_on = True
--- paddle.py
+++ paddle.py
@@ -1,22 +1,22 @@
-
-# TODO: create moving paddle
 from turtle import Turtle
 STRETCH_W = 5
 STRETCH_L = 1
 class Paddle(Turtle):
     def __init__(self, x, y) -> None:
-        self.shape("rectangle")
+        super().__init__()
+        self.shape("square")
         self.color('white')
-        self.up()
+        self.penup()
         self.shapesize(stretch_wid=5,stretch_len=1)
         self.goto(x, y)
         
 
 
 # Moving
-    def up(self):
-        self.goto(self.xcor(), self.ycor()+20)
+    def go_up(self):
+        new_y = self.ycor()+20
+        self.goto(self.xcor(), new_y)
     
 
-    def down(self):
+    def go_down(self):
         self.goto(self.xcor(), self.ycor()-20)

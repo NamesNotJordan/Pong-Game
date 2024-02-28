cc3518838d71c555ebf4399cb5549192f92777eb
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-02-28 22:40:54 +0200

    setup screen and paddle class

--- main.py
+++ main.py
@@ -6,15 +6,23 @@
 # TODO: Create moving ball
 # TODO: detect paddle miss
 from turtle import Screen
-
+from paddle import Paddle
 # Setting Up Screen
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=800)
+screen.title("Pong")
+screen.tracer(0)
+
+
+# Create Paddles
+right_paddle = Paddle(350, 0)
+left_paddle = Paddle(-350, 0)
+
 
 # Gameplay
 game_is_on = True
 while game_is_on:
-    pass
+    screen.update()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,16 +1,22 @@
 
 # TODO: create moving paddle
 from turtle import Turtle
-
-class Paddle:
-    def __init__(self) -> None:
-        pass
+STRETCH_W = 5
+STRETCH_L = 1
+class Paddle(Turtle):
+    def __init__(self, x, y) -> None:
+        self.shape("rectangle")
+        self.color('white')
+        self.up()
+        self.shapesize(stretch_wid=5,stretch_len=1)
+        self.goto(x, y)
+        
 
 
 # Moving
     def up(self):
-        pass
+        self.goto(self.xcor(), self.ycor()+20)
     
 
     def down(self):
-        pass
+        self.goto(self.xcor(), self.ycor()-20)
